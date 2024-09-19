1c41767aa9fa59293ca9e3fa886fc4b01b8d02c5
Seema-476 <simmybishnoi29085@gmail.com>
2024-09-19 10:22:09 +0530

    react practice

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+        <>
+            <div className="text-red-400 text-[25px]">
+                hello world
+            </div>
+        </>
+      )
+}
+export default Header
